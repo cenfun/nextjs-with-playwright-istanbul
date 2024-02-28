824d655679038170104651683f96ab5b75912b40
cenfun <cenfun@gmail.com>
2024-02-28 09:25:45 +0800

    fix issue #2

--- e2e/fixtures.js
+++ e2e/fixtures.js
@@ -4,9 +4,7 @@ import { addCoverageReport } from 'monocart-reporter';
 // fixtures
 // const test = testBase.extend({
 //     autoTestFixture: [async ({ page }, use) => {
-
 //         const isChromium = test.info().project.name === 'Desktop Chrome';
-
 //         // console.log('autoTestFixture setup...', test.info().project.name);
 
 //         await use('autoTestFixture');
@@ -30,18 +28,20 @@ import { addCoverageReport } from 'monocart-reporter';
 // see issue: https://github.com/cenfun/nextjs-with-playwright-istanbul/issues/2
 const test = testBase.extend({
     context: async ({ context }, use) => {
+
         await context.addInitScript(() => window.addEventListener('beforeunload', () => window.collectIstanbulCoverage(window.__coverage__)));
         await context.exposeFunction('collectIstanbulCoverage', (coverage) => {
             if (coverage) {
                 addCoverageReport(coverage, test.info());
             }
         });
+
         await use(context);
-        // eslint-disable-next-line no-restricted-syntax
+
         for (const page of context.pages()) {
-        // eslint-disable-next-line no-await-in-loop
             await page.evaluate(() => window.collectIstanbulCoverage(window.__coverage__));
         }
+
     }
 });
 
