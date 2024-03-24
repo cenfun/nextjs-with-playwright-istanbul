e3678130a2dff494203d3c0837edff1e584936f6
cenfun <cenfun@gmail.com>
2024-03-24 11:12:48 +0800

    updated CDPClient

--- global-teardown.js
+++ global-teardown.js
@@ -1,31 +1,18 @@
-import CDP from 'chrome-remote-interface';
 import { addCoverageReport } from 'monocart-reporter';
+import { CDPClient } from 'monocart-coverage-reports';
 
 const globalTeardown = async (config) => {
     console.log('globalTeardown ...');
 
     // [WebServer] the --inspect option was detected, the Next.js router server should be inspected at port 9230.
-    const client = await CDP({
+    const client = await CDPClient({
         port: 9230
     });
 
-    // enable and start v8 coverage
-    await client.Runtime.enable();
-
-    // write the coverage started by NODE_V8_COVERAGE to disk on demand
-    const data = await client.Runtime.evaluate({
-        expression: 'new Promise(resolve=>{ resolve(global.__coverage__); })',
-        includeCommandLineAPI: true,
-        returnByValue: true,
-        awaitPromise: true
-    });
-
-    await client.Runtime.disable();
-
-    // close debugger
+    const coverageData = await client.getIstanbulCoverage();
     await client.close();
 
-    const coverageData = data.result.value;
+    // console.log(Object.keys(coverageData));
 
     // there is no test info on teardown, just mock one with required config
     const mockTestInfo = {
